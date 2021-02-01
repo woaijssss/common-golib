b9051d8a444f79db115bc58da86c8419a5d88065
woaijssss <wenhanf@163.com>
2021-02-01 21:10:15 +0800

    提交k8s监控服务库

---
+++ ginprom/example/server/server.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/woaijssss/ginprom"
+)
+
+func init() {
+	rand.Seed(time.Now().Unix())
+}
+
+func zzZ() {
+	time.Sleep(time.Millisecond * time.Duration(rand.Int()%1000))
+}
+
+func main() {
+	r := gin.Default()
+	r.Use(ginprom.PromMiddleware(nil))
+
+	r.GET("/metrics", ginprom.PromHandler(promhttp.Handler()))
+
+	r.GET("/", func(c *gin.Context) {
+		zzZ()
+		c.JSON(http.StatusOK, gin.H{
+			"message": "home",
+		})
+	})
+
+	r.GET("/index", func(c *gin.Context) {
+		zzZ()
+		c.JSON(http.StatusOK, gin.H{
+			"message": "index",
+		})
+	})
+
+	r.GET("/forbidden", func(c *gin.Context) {
+		zzZ()
+		c.AbortWithStatusJSON(http.StatusForbidden, gin.H{
+			"message": "forbidden",
+		})
+	})
+
+	r.GET("/badreq", func(c *gin.Context) {
+		zzZ()
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"message": "badreq",
+		})
+	})
+
+	if err := r.Run(":4433"); err != nil {
+		log.Fatalf("run server error: %v", err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/woaijssss/common-golib
+
+go 1.15
+
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/prometheus/client_golang v1.9.0
+)
